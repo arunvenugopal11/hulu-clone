4cea68843b0bbc78e2d44aa1393c33d55ccdebdd
Arun <arunvenugopal11@gmail.com>
2021-11-18 17:54:40 +0530

    alignment fix

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -12,7 +12,7 @@ const Thumbnail = ({result}) => {
                 height={1080}/>
                 <div className="p-2">
                     <p className="truncate max-w-md">{result.overview}</p>
-                    <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
+                    <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold truncate">{result.title || result.original_name}</h2>
                     <p className="flex items-center opacity-0 group-hover:opacity-100">
                         {result.media_type && `${result.media_type} .`}{" "}
                         {result.release_date || result.first_air_date} . {" "}
